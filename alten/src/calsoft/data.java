42cfc9484a73101841710210303a4c5e87b12f59
chandhu <chandhu@chandhu-VirtualBox>
2017-10-10 15:17:25 +0530

    initial checking

---
+++ alten/src/calsoft/data.java
@@ -0,0 +1,9 @@
+package calsoft;
+
+public class data {
+
+    public void display() {
+        System.out.println("new user");
+    }
+
+}
---
+++ alten/src/calsoft/labs.java
@@ -0,0 +1,15 @@
+package calsoft;
+
+public class labs {
+
+    public static void main(String args[]) {
+        data d = new data();
+        display();
+        d.display();
+    }
+
+    private static void display() {
+        System.out.println("checking");
+        System.out.println("is he new user");
+    }
+}
